19fd477ba73a2b1f61bff8c9b4d86bd920c7a720
RakashB <93943051+skyblue05@users.noreply.github.com>
2023-12-19 18:20:03 +0530

    First

---
+++ script.js
@@ -0,0 +1,27 @@
+let score = 10;
+let chances = 0;
+let userGuessInput = document.getElementById("userGuess");
+let ok = document.getElementById("ok")
+
+let number = Math.floor((Math.random)()*100 +1);
+
+function check(){
+    const userGuess = parseInt(userGuessInput.value);
+    const scorePlayer = score - chances;
+
+    if (userGuess == number){
+        ok.textContent = "Congratulations 🤩 You Did it 🏆. Your Score is "+ scorePlayer + "Out of 10, Keep it up 👍, Try Your Highest";
+    } else if (userGuess > number) {
+        ok.textContent = "Too High, Try Again 😮";
+        chances++;
+    } else if(userGuess < number) {
+        ok.textContent= "Too Low, Try Again 👎" ;
+        chances++;
+    } else {
+        ok.textContent= "Invalid Value" ;
+    }
+    userGuessInput.value = "";
+
+}
+
+        
